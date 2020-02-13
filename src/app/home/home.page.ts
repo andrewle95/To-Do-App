88cac52dd060eecfe5284e775766a33a3bdbac0b
andrewle95 <leaa1@uci.edu>
2020-02-13 01:03:08 -0800

    Setting up Home Page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  test = "Hello";
+  tasks = ["Hello123", "Hello1234"];
 
   constructor() {}
 
+  public addTask() {
+    this.test = "Hi";
+    console.log("I Added it");
+  }
 }
